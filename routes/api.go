1cb5da9afcea6591b89447128b9256b0863765b5
ephemeral98 <3502969036@qq.com>
2023-07-14 17:51:52 +0800

    ping

--- routes/api.go
+++ routes/api.go
@@ -1,21 +1,27 @@
 package routes
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/jassue/jassue-gin/app/controllers/app"
-    "github.com/jassue/jassue-gin/app/controllers/common"
-    "github.com/jassue/jassue-gin/app/middleware"
-    "github.com/jassue/jassue-gin/app/services"
+	"github.com/gin-gonic/gin"
+	"github.com/jassue/jassue-gin/app/controllers/app"
+	"github.com/jassue/jassue-gin/app/controllers/common"
+	"github.com/jassue/jassue-gin/app/middleware"
+	"github.com/jassue/jassue-gin/app/services"
 )
 
 func SetApiGroupRoutes(router *gin.RouterGroup) {
-    router.POST("/auth/register", app.Register)
-    router.POST("/auth/login", app.Login)
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
-    authRouter := router.Group("").Use(middleware.JWTAuth(services.AppGuardName))
-    {
-        authRouter.POST("/auth/info", app.Info)
-        authRouter.POST("/auth/logout", app.Logout)
-        authRouter.POST("/image_upload", common.ImageUpload)
-    }
+	router.POST("/auth/register", app.Register)
+	router.POST("/auth/login", app.Login)
+
+	authRouter := router.Group("").Use(middleware.JWTAuth(services.AppGuardName))
+	{
+		authRouter.POST("/auth/info", app.Info)
+		authRouter.POST("/auth/logout", app.Logout)
+		authRouter.POST("/image_upload", common.ImageUpload)
+	}
 }
